3b9e941953d6acc4fbf7bd4cda7b189ddc2e8ed7
Raayhan <rayhan.rakib@northsouth.edu>
2024-07-10 15:11:55 +0600

    Create Vue project with tailwind

---
+++ lets-learn_frontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-lg text-red-800 font-bold italic">Home Page</div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
