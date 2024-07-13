944349f4c85683c32bed56c3e317c3fae495997b
Raayhan <rayhan.rakib@northsouth.edu>
2024-07-13 11:22:45 +0600

    Added Category & course model & DB

--- LetsLearn_backend/course/admin.py
+++ LetsLearn_backend/course/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category,Course
+from ckeditor.widgets import CKEditorWidget
+from django import forms
 
 
 class CategoryList(admin.ModelAdmin):
     list_display = ("name","slug")
 
+
+class CourseForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class CourseAdmin(admin.ModelAdmin):
+    form = CourseForm
+    
+
+class CourseList(admin.ModelAdmin):
+    list_display = ("title","category","author","slug","date_added")
+
 admin.site.register(Category,CategoryList)
+admin.site.register(Course,CourseList)
--- LetsLearn_backend/course/models.py
+++ LetsLearn_backend/course/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -16,3 +17,26 @@ class Category(models.Model):
     
     def get_absolute_url(self):
         return f'/{self.slug}/'
+
+class Course(models.Model):
+    
+    category    = models.ForeignKey(Category,related_name='courses', on_delete=models.CASCADE)
+    title       = models.CharField(max_length=100)
+    summary     = models.CharField(max_length=255)
+    slug        = models.SlugField()
+    author      = models.ForeignKey(User,related_name='courses', on_delete=models.CASCADE)
+    description = RichTextField()
+    date_added  = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
